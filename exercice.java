3f19a26b8bef2cebb9d95e44dbf95b3badad7c41
Laissad <laissadaboulaith128@gmail.com>
2025-02-04 18:08:01 +0100

    Ajout exercices

---
+++ exercice.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+
+public class exercice {
+    public static void main(String[] args) {
+        int[] nombre= {12, 7, 22, 5, 17, 9, 14, 30, 25, 8};
+
+        System.out.println("Tableau initial: " + Arrays.toString(nombre));
+
+        // Trier le tableau
+        
+        System.out.println("Tableau trié: " + Arrays.toString(nombre));
+
+        // Trouver min et max
+        int min = nombre[0];
+        int max = nombre[nombre.length - 1];
+        System.out.println("Valeur minimale: " + min);
+        System.out.println("Valeur maximale: " + max);
+
+        // Calcul de la moyenne
+        double moyenne = calculerMoyenne(nombre);
+        System.out.println("Moyenne: " + moyenne);
+
+        // Calcul de la médiane
+        double mediane = calculerMediane(nombre);
+        System.out.println("Médiane: " + mediane);
+
+        // Calcul de l'écart-type
+        double ecartType = calculerEcartType(nombre, moyenne);
+        System.out.println("Écart-type: " + ecartType);
+    }
+
+    public static double calculerMoyenne(int[] tableau) {
+        double somme = 0;
+        for (int num : tableau) {
+            somme += num;
+        }
+        return somme / tableau.length;
+    }
+
+    public static double calculerMediane(int[] tableau) {
+        int n = tableau.length;
+        if (n % 2 == 0) {
+            return (tableau[n / 2 - 1] + tableau[n / 2]) / 2.0;
+        } else {
+            return tableau[n / 2];
+        }
+    }
+
+    public static double calculerEcartType(int[] tableau, double moyenne) {
+        double somme = 0;
+        for (int num : tableau) {
+            somme += Math.pow(num - moyenne, 2);
+        }
+        return Math.sqrt(somme / tableau.length);
+    }
+}
---
+++ exo.java
@@ -0,0 +1,12 @@
+public class exo {
+    public static void main(String[] args) {
+
+        //creer un compte à rebours qui compte à rebours de 10 à 1 apres avoir affiche "lancement"
+        System.out.println("lancement");
+        for (int i = 10; i >= 1; i--) {
+            
+            System.out.println(i);
+            }
+        }
+             
+}
