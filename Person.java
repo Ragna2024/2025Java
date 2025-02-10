dbecb671454049a2c9b9203fa5bbac28df0e5915
Laissad <laissadaboulaith128@gmail.com>
2025-02-10 17:50:34 +0100

    ajout ligne solde

---
+++ Person.java
@@ -0,0 +1,19 @@
+public class Person {
+string nom;
+int âge;
+
+
+public Person()
+
+public Person(string nom, int age)
+
+
+
+public void sePresenter() {
+    System.out.println("Bonjour, je m'appelle " + nom + " et j'ai " +
+    âge + " ans");
+    }
+
+
+
+}
---
+++ Voiture1.java
@@ -0,0 +1,9 @@
+public class Voiture1 {
+    
+    private String marque;
+    private String modele;
+    private string couleur;
+}
+
+
+
